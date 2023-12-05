d4f8504597ea0745a15970f843c092e8479bd6b4
TA1154 <wahdini.a.ft21@mail.umy.ac.id>
2023-12-05 23:58:06 +0700

    menambahkan object DestinasiHome

--- app/src/main/java/com/example/project7/ui/HalamanHome.kt
+++ app/src/main/java/com/example/project7/ui/HalamanHome.kt
@@ -1,2 +1,10 @@
 package com.example.project7.ui
 
+import com.example.project7.navigasi.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
+
+@
