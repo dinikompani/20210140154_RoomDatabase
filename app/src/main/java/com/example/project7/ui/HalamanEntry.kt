19b3ae6b2dcb54bf42a4371faf6f3edf0320abdf
TA1154 <wahdini.a.ft21@mail.umy.ac.id>
2023-12-05 23:03:09 +0700

    menambahkan object DestinasiEntry

--- app/src/main/java/com/example/project7/ui/HalamanEntry.kt
+++ app/src/main/java/com/example/project7/ui/HalamanEntry.kt
@@ -1,2 +1,8 @@
 package com.example.project7.ui
 
+import com.example.project7.navigasi.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
