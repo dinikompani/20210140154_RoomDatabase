dacb7637b48986b7dc6d0e730de0d1ffa83f5918
TA1154 <wahdini.a.ft21@mail.umy.ac.id>
2023-12-05 22:50:15 +0700

    menambahkan object PenyediaViewModel

--- app/src/main/java/com/example/project7/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/project7/model/PenyediaViewModel.kt
@@ -1,2 +1,25 @@
 package com.example.project7.model
 
+import android.app.Application
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.project7.AplikasiSiswa
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
+
+fun CreationExtras.aplikasiSiswa():ApilkasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
