522c17a983444b3fc10de83f0e3708d5bd9fc8eb
TA1154 <wahdini.a.ft21@mail.umy.ac.id>
2023-12-05 22:38:47 +0700

    menambahkan class AplikasiSiswa

--- app/src/main/java/com/example/project7/AplikasiSiswa.kt
+++ app/src/main/java/com/example/project7/AplikasiSiswa.kt
@@ -1,4 +1,16 @@
 package com.example.project7
 
-class AplikasiSiswa {
+import android.app.Application
+import com.example.project7.data.DatabaseSiswa
+import com.example.project7.repositori.ContainerApp
+import com.example.project7.repositori.ContainerDataApp
+
+class AplikasiSiswa : Application() {
+
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
 }
